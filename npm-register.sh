d1371a9b46ca1b88722c95d0ca6fedb7d28b6103
Paris Qian Sen <qiansen1386@gmail.com>
2016-11-22 15:49:35 +0800

    Add & update useful shell commands

--- npm-register.sh
+++ npm-register.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-# If use Ubuntu Bash on Windows(make sure a linux-ver Node has installed)
-# export PATH="$PATH:$(npm bin)"
-# If use git bash/mitty. npm bin will return a win-format path
-export PATH="./node_modules/.bin:$PATH"
+# 1st command is recommaned 
+# For Ubuntu Bash on Windows/POSIX environment
+export PATH="$PATH:$(npm bin)"
+# If use git bash/mitty on Windows. $(npm bin) will return a unsupported win-format path
+# However putting node_modules/ in front may cause security risks.
+#export PATH="./node_modules/.bin:$PATH" # Lagancy method, DO NOT use for production
